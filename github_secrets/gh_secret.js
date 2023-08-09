f44d6bfd714f14534c121e5f5ffeb1081130d215
devonrmcclure <devon+github@devonmcclure.dev>
2023-08-09 08:23:04 -0600

    add link to docs reference

--- github_secrets/gh_secret.js
+++ github_secrets/gh_secret.js
@@ -1,24 +1,24 @@
 //Reuired: npm install libsodium-wrappers
 
-const sodium = require('libsodium-wrappers');
+const sodium = require("libsodium-wrappers");
 
 var args = process.argv.slice(2);
 
 secret = args[0];
-key = args[1]
+key = args[1];
 
-// From GH API Docs.
+// From GH API Docs: https://docs.github.com/en/rest/guides/encrypting-secrets-for-the-rest-api?apiVersion=2022-11-28
 //Check if libsodium is ready and then proceed.
 sodium.ready.then(() => {
   // Convert Secret & Base64 key to Uint8Array.
-  let binkey = sodium.from_base64(key, sodium.base64_variants.ORIGINAL)
-  let binsec = sodium.from_string(secret)
+  let binkey = sodium.from_base64(key, sodium.base64_variants.ORIGINAL);
+  let binsec = sodium.from_string(secret);
 
   //Encrypt the secret using LibSodium
-  let encBytes = sodium.crypto_box_seal(binsec, binkey)
+  let encBytes = sodium.crypto_box_seal(binsec, binkey);
 
   // Convert encrypted Uint8Array to Base64
-  let output = sodium.to_base64(encBytes, sodium.base64_variants.ORIGINAL)
+  let output = sodium.to_base64(encBytes, sodium.base64_variants.ORIGINAL);
 
-  console.log(output)
+  console.log(output);
 });
